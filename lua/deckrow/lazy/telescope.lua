1b382b7bcf44d2a3d49b3b6503703bfc2ff4c807
Denys Maftulyak <denysmaftulyak@Denyss-MacBook-Pro.local>
2026-01-31 17:42:53 +0200

    refactor(telescope): move the telescope config logic to the telescope-config class

--- lua/deckrow/lazy/telescope.lua
+++ lua/deckrow/lazy/telescope.lua
@@ -1,105 +1,14 @@
+local TelescopeConfig = require("deckrow.lib.telescope-config")
+
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope-ui-select.nvim'
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-ui-select.nvim"
   },
-  opts = {
-    extensions = {
-      ["ui-select"] = {
-        require("telescope.themes").get_dropdown({
-          layout_config = {
-            width = 0.8,
-            height = 20,
-            prompt_position = "bottom",
-          },
-        }),
-      },
-    },
-    defaults = {
-      layout_strategy = "vertical",
-      previewer = false,
-      path_display = { "relative" },
-
-      layout_config = {
-        preview_cutoff = 0
-      },
-
-      mappings = {
-        i = {
-          ["<C-j>"] = require('telescope.actions').move_selection_next,
-          ["<C-k>"] = require('telescope.actions').move_selection_previous,
-          ["<Tab>"] = function(prompt_bufnr)
-            local picker = require('telescope.actions.state').get_selected_entry(prompt_bufnr)
-            require('telescope.actions').select_default(prompt_bufnr)
-          end,
-        },
-      }
-    }
-  },
-  config = function(_, opts)
-    require('telescope').setup(opts)
-    -- require('telescope').load_extension('fzf')
-    require('telescope').load_extension('ui-select')
+  config = function()
+    TelescopeConfig:setup(TelescopeConfig:get_opts())
   end,
-  keys = {
-    { "<leader>pr", function() require("telescope.builtin").oldfiles() end,    desc = "Recent Files" },
-    { "<leader>pb", function() require("telescope.builtin").buffers() end,     desc = "Buffers" },
-    { "<leader>ps", function() require("telescope.builtin").live_grep() end,   desc = "Live Grep" },
-    { "<leader>pc", function() require("telescope.builtin").grep_string() end, desc = "Grep Current Word" },
-    { "<leader>pg", function() require("telescope.builtin").git_status() end,  desc = "Git Status" },
-    { "<leader>gb", "<cmd>Telescope git_branches<cr>", desc = "Git Branches" },
-    {
-      "<leader>pf",
-      function()
-        require("telescope.builtin").find_files({
-          find_command = {
-            "fd",
-            "--type", "f",
-            "--hidden",
-            "--no-ignore",
-            "--exclude", ".git",
-            "--exclude", "node_modules",
-            "--exclude", "dist",
-            "--exclude", "build",
-          }
-        })
-      end,
-      desc = "Find Files"
-    },
-    {
-      "<leader>pF",
-      function()
-        require("telescope.builtin").find_files({
-          prompt_title = "Find Directory",
-          find_command = { "fd", "--type", "d", "--hidden", "--exclude", ".git" }
-        })
-      end,
-      desc = "Git Status"
-    },
-    -- alternative:
-    -- { "<leader>pg", function() require("telescope.builtin").git_files({ show_untracked = false }) end, desc = "Git Files" },
-
-    {
-      "<leader>pws",
-      function()
-        local word = vim.fn.expand("<cword>")
-        require("telescope.builtin").grep_string({ search = word })
-      end,
-      desc = "Grep Word (cword)"
-    },
-
-    {
-      "<leader>pWs",
-      function()
-        local word = vim.fn.expand("<cWORD>")
-        require("telescope.builtin").grep_string({ search = word })
-      end,
-      desc = "Grep WORD (cWORD)"
-    },
-
-    { "<leader>vh", function() require("telescope.builtin").help_tags() end, desc = "Help Tags" },
-    { "<leader>pl", function() require("telescope.builtin").lsp_document_symbols() end, desc = "LSP Document Symbols" },
-  }
+  keys = TelescopeConfig:get_keys()
 }
---
+++ lua/deckrow/lib/telescope-config.lua
@@ -0,0 +1,126 @@
+local TelescopeConfig = {}
+
+TelescopeConfig.excluded_dirs = { ".git", "node_modules", "dist", "build" }
+
+function TelescopeConfig:setup(opts)
+  require("telescope").setup(opts)
+  require("telescope").load_extension("ui-select")
+end
+
+function TelescopeConfig:get_opts()
+  local actions = require("telescope.actions")
+  local themes = require("telescope.themes")
+
+  return {
+    extensions = {
+      ["ui-select"] = {
+        themes.get_dropdown({
+          layout_config = {
+            width = 0.8,
+            height = 20,
+            prompt_position = "bottom",
+          },
+        }),
+      },
+    },
+    defaults = {
+      layout_strategy = "vertical",
+      previewer = false,
+      path_display = { "relative" },
+      layout_config = {
+        preview_cutoff = 0
+      },
+      mappings = {
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<Tab>"] = function(prompt_bufnr)
+            actions.select_default(prompt_bufnr)
+          end,
+        },
+      }
+    }
+  }
+end
+
+function TelescopeConfig:get_keys()
+  return {
+    { "<leader>pr", function() self:oldfiles() end, desc = "Recent Files" },
+    { "<leader>pb", function() self:buffers() end, desc = "Buffers" },
+    { "<leader>ps", function() self:live_grep() end, desc = "Live Grep" },
+    { "<leader>pc", function() self:grep_string() end, desc = "Grep Current Word" },
+    { "<leader>pg", function() self:git_status() end, desc = "Git Status" },
+    { "<leader>gb", "<cmd>Telescope git_branches<cr>", desc = "Git Branches" },
+    { "<leader>pf", function() self:find_files() end, desc = "Find Files" },
+    { "<leader>pF", function() self:find_directories() end, desc = "Find Directory" },
+    { "<leader>pws", function() self:grep_cword() end, desc = "Grep Word (cword)" },
+    { "<leader>pWs", function() self:grep_cWORD() end, desc = "Grep WORD (cWORD)" },
+    { "<leader>vh", function() self:help_tags() end, desc = "Help Tags" },
+    { "<leader>pl", function() self:lsp_document_symbols() end, desc = "LSP Document Symbols" },
+  }
+end
+
+function TelescopeConfig:oldfiles()
+  require("telescope.builtin").oldfiles()
+end
+
+function TelescopeConfig:buffers()
+  require("telescope.builtin").buffers()
+end
+
+function TelescopeConfig:live_grep()
+  require("telescope.builtin").live_grep()
+end
+
+function TelescopeConfig:grep_string()
+  require("telescope.builtin").grep_string()
+end
+
+function TelescopeConfig:git_status()
+  require("telescope.builtin").git_status()
+end
+
+function TelescopeConfig:find_files()
+  local find_command = {
+    "fd",
+    "--type", "f",
+    "--hidden",
+    "--no-ignore",
+  }
+
+  for _, dir in ipairs(self.excluded_dirs) do
+    table.insert(find_command, "--exclude")
+    table.insert(find_command, dir)
+  end
+
+  require("telescope.builtin").find_files({
+    find_command = find_command
+  })
+end
+
+function TelescopeConfig:find_directories()
+  require("telescope.builtin").find_files({
+    prompt_title = "Find Directory",
+    find_command = { "fd", "--type", "d", "--hidden", "--exclude", ".git" }
+  })
+end
+
+function TelescopeConfig:grep_cword()
+  local word = vim.fn.expand("<cword>")
+  require("telescope.builtin").grep_string({ search = word })
+end
+
+function TelescopeConfig:grep_cWORD()
+  local word = vim.fn.expand("<cWORD>")
+  require("telescope.builtin").grep_string({ search = word })
+end
+
+function TelescopeConfig:help_tags()
+  require("telescope.builtin").help_tags()
+end
+
+function TelescopeConfig:lsp_document_symbols()
+  require("telescope.builtin").lsp_document_symbols()
+end
+
+return TelescopeConfig
