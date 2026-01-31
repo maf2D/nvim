fa9546cbbdfee7631531ff62bab6c91ef970d75e
Denys Maftulyak <denysmaftulyak@Denyss-MacBook-Pro.local>
2026-01-31 16:20:19 +0200

    feat(harpoon): update styles and change keymap sets

--- lua/deckrow/lazy/harpoon.lua
+++ lua/deckrow/lazy/harpoon.lua
@@ -4,22 +4,26 @@ return {
   branch = 'harpoon2',
   config = function()
     local harpoon = require("harpoon")
+    local toggle_harpoon_menu_with_custom_settings
 
     harpoon:setup()
 
+    vim.keymap.set("n", "<leader>h", function() toggle_harpoon_menu_with_custom_settings() end)
     vim.keymap.set("n", "<leader>a", function() harpoon:list():prepend() end)
     vim.keymap.set("n", "<leader>A", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-m>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-m>", function() harpoon:list():select(2) end)
     vim.keymap.set("n", "<leader><C-j>", function() harpoon:list():replace_at(1) end)
     vim.keymap.set("n", "<leader><C-k>", function() harpoon:list():replace_at(2) end)
-    vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-    vim.keymap.set("n", "<leader><C-m>", function() harpoon:list():replace_at(4) end)
+
+    -- helpers
+    toggle_harpoon_menu_with_custom_settings = function()
+      harpoon.ui:toggle_quick_menu(harpoon:list(), {
+        border = "rounded",
+        title = { { " Harpoon ", "TelescopePreviewTitle" } },
+        title_pos = "center",
+        ui_width_ratio = 0.6,
+      })
+    end
   end
 }
-
-
